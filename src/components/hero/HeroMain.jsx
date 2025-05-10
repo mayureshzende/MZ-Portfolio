500ffe41eb8e66165b513e42d9b9c60139d4fab0
mayureshzende <mayureshzende@gmail.com>
2025-05-10 23:56:43 +0530

    fixing overflow isssues and code clean up

--- src/components/hero/HeroMain.jsx
+++ src/components/hero/HeroMain.jsx
@@ -5,7 +5,7 @@ const HeroMain = () => {
   return (
     <div className="pt-20 pb-14">
       {/* to remove left right pading while finishing */}
-      <div className="flex md:flex-row sm:flex-col max-w-[1200px] mx-auto justify-between items-center relative pr-10 pl-5 ">
+      <div className="flex md:flex-row sm:flex-col max-w-[1200px] mx-auto justify-between items-center relative pr-10 pl-5 overflow-hidden">
         <HeroText />
         <HeroPic />
       </div>
